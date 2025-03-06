5a24103124bf3aeb0142b0a1f4a891aca0704d44
Hamdy <hamdey.elattar@gmail.com>
2025-03-06 20:24:43 +0200

    prof

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2467106356"
+    serviceWorkerVersion: "56765814"
   }
 });
